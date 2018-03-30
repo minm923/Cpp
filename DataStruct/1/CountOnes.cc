893e06b3afbcc76df8bae74e8db7bf7bc9a992e6
minm923 <minm923@126.com>
2018-03-30 10:29:16 +0800

    CountOnes

--- DataStruct/1/CountOnes.cc
+++ DataStruct/1/CountOnes.cc
@@ -2,6 +2,19 @@
 
 // log(n)
 
+int CountOnes(int n)
+{
+    if (n < 0) return -1;
+
+    int sum = 0;
+    while (n)
+    {
+        sum += (n & 1);
+        n >>= 1;
+    }
+
+}
+
 int main (int argc, char * argv[])
 {
 
